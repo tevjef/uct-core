7fe9c3cd49bf71a082ba22804c25efe9b22811c9
Tevin Jeffrey <tev.jeffrey@gmail.com>
2016-05-05 00:55:07 -0400

    Fix cross compilation errors

--- common/model_ffjson.go
+++ common/model_ffjson.go
@@ -6837,255 +6837,6 @@ done:
 	return nil
 }
 
-func (mj *UctSemester) MarshalJSON() ([]byte, error) {
-	var buf fflib.Buffer
-	if mj == nil {
-		buf.WriteString("null")
-		return buf.Bytes(), nil
-	}
-	err := mj.MarshalJSONBuf(&buf)
-	if err != nil {
-		return nil, err
-	}
-	return buf.Bytes(), nil
-}
-func (mj *UctSemester) MarshalJSONBuf(buf fflib.EncodingBuffer) error {
-	if mj == nil {
-		buf.WriteString("null")
-		return nil
-	}
-	var err error
-	var obj []byte
-	_ = obj
-	_ = err
-	buf.WriteString(`{ `)
-	if mj.Year != 0 {
-		buf.WriteString(`"year":`)
-		fflib.FormatBits2(buf, uint64(mj.Year), 10, mj.Year < 0)
-		buf.WriteByte(',')
-	}
-	if len(mj.Season) != 0 {
-		buf.WriteString(`"season":`)
-		fflib.WriteJsonString(buf, string(mj.Season))
-		buf.WriteByte(',')
-	}
-	buf.Rewind(1)
-	buf.WriteByte('}')
-	return nil
-}
-
-const (
-	ffj_t_UctSemesterbase = iota
-	ffj_t_UctSemesterno_such_key
-
-	ffj_t_UctSemester_Year
-
-	ffj_t_UctSemester_Season
-)
-
-var ffj_key_UctSemester_Year = []byte("year")
-
-var ffj_key_UctSemester_Season = []byte("season")
-
-func (uj *UctSemester) UnmarshalJSON(input []byte) error {
-	fs := fflib.NewFFLexer(input)
-	return uj.UnmarshalJSONFFLexer(fs, fflib.FFParse_map_start)
-}
-
-func (uj *UctSemester) UnmarshalJSONFFLexer(fs *fflib.FFLexer, state fflib.FFParseState) error {
-	var err error = nil
-	currentKey := ffj_t_UctSemesterbase
-	_ = currentKey
-	tok := fflib.FFTok_init
-	wantedTok := fflib.FFTok_init
-
-mainparse:
-	for {
-		tok = fs.Scan()
-		//	println(fmt.Sprintf("debug: tok: %v  state: %v", tok, state))
-		if tok == fflib.FFTok_error {
-			goto tokerror
-		}
-
-		switch state {
-
-		case fflib.FFParse_map_start:
-			if tok != fflib.FFTok_left_bracket {
-				wantedTok = fflib.FFTok_left_bracket
-				goto wrongtokenerror
-			}
-			state = fflib.FFParse_want_key
-			continue
-
-		case fflib.FFParse_after_value:
-			if tok == fflib.FFTok_comma {
-				state = fflib.FFParse_want_key
-			} else if tok == fflib.FFTok_right_bracket {
-				goto done
-			} else {
-				wantedTok = fflib.FFTok_comma
-				goto wrongtokenerror
-			}
-
-		case fflib.FFParse_want_key:
-			// json {} ended. goto exit. woo.
-			if tok == fflib.FFTok_right_bracket {
-				goto done
-			}
-			if tok != fflib.FFTok_string {
-				wantedTok = fflib.FFTok_string
-				goto wrongtokenerror
-			}
-
-			kn := fs.Output.Bytes()
-			if len(kn) <= 0 {
-				// "" case. hrm.
-				currentKey = ffj_t_UctSemesterno_such_key
-				state = fflib.FFParse_want_colon
-				goto mainparse
-			} else {
-				switch kn[0] {
-
-				case 's':
-
-					if bytes.Equal(ffj_key_UctSemester_Season, kn) {
-						currentKey = ffj_t_UctSemester_Season
-						state = fflib.FFParse_want_colon
-						goto mainparse
-					}
-
-				case 'y':
-
-					if bytes.Equal(ffj_key_UctSemester_Year, kn) {
-						currentKey = ffj_t_UctSemester_Year
-						state = fflib.FFParse_want_colon
-						goto mainparse
-					}
-
-				}
-
-				if fflib.EqualFoldRight(ffj_key_UctSemester_Season, kn) {
-					currentKey = ffj_t_UctSemester_Season
-					state = fflib.FFParse_want_colon
-					goto mainparse
-				}
-
-				if fflib.SimpleLetterEqualFold(ffj_key_UctSemester_Year, kn) {
-					currentKey = ffj_t_UctSemester_Year
-					state = fflib.FFParse_want_colon
-					goto mainparse
-				}
-
-				currentKey = ffj_t_UctSemesterno_such_key
-				state = fflib.FFParse_want_colon
-				goto mainparse
-			}
-
-		case fflib.FFParse_want_colon:
-			if tok != fflib.FFTok_colon {
-				wantedTok = fflib.FFTok_colon
-				goto wrongtokenerror
-			}
-			state = fflib.FFParse_want_value
-			continue
-		case fflib.FFParse_want_value:
-
-			if tok == fflib.FFTok_left_brace || tok == fflib.FFTok_left_bracket || tok == fflib.FFTok_integer || tok == fflib.FFTok_double || tok == fflib.FFTok_string || tok == fflib.FFTok_bool || tok == fflib.FFTok_null {
-				switch currentKey {
-
-				case ffj_t_UctSemester_Year:
-					goto handle_Year
-
-				case ffj_t_UctSemester_Season:
-					goto handle_Season
-
-				case ffj_t_UctSemesterno_such_key:
-					err = fs.SkipField(tok)
-					if err != nil {
-						return fs.WrapErr(err)
-					}
-					state = fflib.FFParse_after_value
-					goto mainparse
-				}
-			} else {
-				goto wantedvalue
-			}
-		}
-	}
-
-handle_Year:
-
-	/* handler: uj.Year type=int kind=int quoted=false*/
-
-	{
-		if tok != fflib.FFTok_integer && tok != fflib.FFTok_null {
-			return fs.WrapErr(fmt.Errorf("cannot unmarshal %s into Go value for int", tok))
-		}
-	}
-
-	{
-
-		if tok == fflib.FFTok_null {
-
-		} else {
-
-			tval, err := fflib.ParseInt(fs.Output.Bytes(), 10, 64)
-
-			if err != nil {
-				return fs.WrapErr(err)
-			}
-
-			uj.Year = int(tval)
-
-		}
-	}
-
-	state = fflib.FFParse_after_value
-	goto mainparse
-
-handle_Season:
-
-	/* handler: uj.Season type=string kind=string quoted=false*/
-
-	{
-
-		{
-			if tok != fflib.FFTok_string && tok != fflib.FFTok_null {
-				return fs.WrapErr(fmt.Errorf("cannot unmarshal %s into Go value for string", tok))
-			}
-		}
-
-		if tok == fflib.FFTok_null {
-
-		} else {
-
-			outBuf := fs.Output.Bytes()
-
-			uj.Season = string(string(outBuf))
-
-		}
-	}
-
-	state = fflib.FFParse_after_value
-	goto mainparse
-
-wantedvalue:
-	return fs.WrapErr(fmt.Errorf("wanted value token, but got token: %v", tok))
-wrongtokenerror:
-	return fs.WrapErr(fmt.Errorf("ffjson: wanted token: %v, but got token: %v output=%s", wantedTok, tok, fs.Output.String()))
-tokerror:
-	if fs.BigError != nil {
-		return fs.WrapErr(fs.BigError)
-	}
-	err = fs.Error.ToError()
-	if err != nil {
-		return fs.WrapErr(err)
-	}
-	panic("ffjson-generated: unreachable, please report bug.")
-done:
-	return nil
-}
-
 func (mj *University) MarshalJSON() ([]byte, error) {
 	var buf fflib.Buffer
 	if mj == nil {
@@ -7868,7 +7619,7 @@ handle_Subjects:
 
 handle_UctSemester:
 
-	/* handler: uj.UctSemester type=[]common.UctSemester kind=slice quoted=false*/
+	/* handler: uj.UctSemester type=[]common.Semester kind=slice quoted=false*/
 
 	{
 
@@ -7882,13 +7633,13 @@ handle_UctSemester:
 			uj.UctSemester = nil
 		} else {
 
-			uj.UctSemester = make([]UctSemester, 0)
+			uj.UctSemester = make([]Semester, 0)
 
 			wantVal := true
 
 			for {
 
-				var tmp_uj__UctSemester UctSemester
+				var tmp_uj__UctSemester Semester
 
 				tok = fs.Scan()
 				if tok == fflib.FFTok_error {
@@ -7909,7 +7660,7 @@ handle_UctSemester:
 					wantVal = true
 				}
 
-				/* handler: tmp_uj__UctSemester type=common.UctSemester kind=struct quoted=false*/
+				/* handler: tmp_uj__UctSemester type=common.Semester kind=struct quoted=false*/
 
 				{
 					if tok == fflib.FFTok_null {
