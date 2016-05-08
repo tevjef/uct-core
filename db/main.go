481630990e1c2be513e11c66ced91fce829c2adb
tevjef <mastermindtj94@gmail.com>
2016-05-08 18:37:42 -0400

    Sync start times

--- db/main.go
+++ db/main.go
@@ -65,6 +65,7 @@ var (
 	sectionCount  int
 	meetingCount  int
 	metadataCount int
+	startTime = time.Now()
 )
 
 func init() {
@@ -672,7 +673,7 @@ func (stats AuditStats) audit() {
 		"db_ops",
 		tags,
 		fields,
-		time.Now(),
+		startTime,
 	)
 
 	uct.CheckError(err)
