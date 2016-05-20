eb3c22a2432ebd11c4498c14bcd40527240a2406
Tevin Jeffrey <tev.jeffrey@gmail.com>
2016-05-20 02:31:29 -0400

    Fix incorrect variable use in bash

--- scripts/rutgers.sh
+++ scripts/rutgers.sh
@@ -9,11 +9,11 @@ campus() {
     LATEST=/tmp/uct/rutgers-${code}-latest.out
     LOG=/var/log/uct/rutgers-${code}.log
     if [ ! -f LATEST ]; then
-        ${path}rutgers -c ${code} -f protobuf | tee ${LATEST} | ${path}db insert-all 2>&1 | tee -a ${LOG}
+        ${path}rutgers -c ${code} -f ${format} | tee ${LATEST} | ${path}db -f ${format} 2>&1 | tee -a ${LOG}
         exit 0
     fi
     cp NB_LATEST NB_OLD
-    ${path}rutgers -c ${code} -f format | tee ${LATEST} | ${path}diff -f format ${OLD} | ${path}db -f format 2>&1 | tee -a ${LOG}
+    ${path}rutgers -c ${code} -f ${format} | tee ${LATEST} | ${path}diff -f ${format} ${OLD} | ${path}db -f ${format} 2>&1 | tee -a ${LOG}
 }
 
 campus CM &
