44fd7e95ba19032d8a80279068bdd09fe69ad1ed
Tevin Jeffrey <tev.jeffrey@gmail.com>
2016-05-22 05:33:22 -0400

    Add logging to diff

--- common/diff/main.go
+++ common/diff/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"fmt"
 	"github.com/gogo/protobuf/proto"
 	"github.com/pquerna/ffjson/ffjson"
 	"gopkg.in/alecthomas/kingpin.v2"
@@ -12,10 +14,11 @@ import (
 )
 
 var (
-	app    = kingpin.New("model-diff", "An application to filter unchanged objects")
-	format = app.Flag("format", "choose file input format").Short('f').HintOptions("protobuf", "json").PlaceHolder("[protobuf, json]").Required().String()
-	old    = app.Arg("old", "the first file to compare").Required().File()
-	new    = app.Arg("new", "the second file to compare").File()
+	app     = kingpin.New("model-diff", "An application to filter unchanged objects")
+	format  = app.Flag("format", "choose file input format").Short('f').HintOptions("protobuf", "json").PlaceHolder("[protobuf, json]").Required().String()
+	old     = app.Arg("old", "the first file to compare").Required().File()
+	new     = app.Arg("new", "the second file to compare").File()
+	verbose = app.Flag("verbose", "Verbose log of object representations.").Short('v').Bool()
 )
 
 func main() {
@@ -80,12 +83,24 @@ func main() {
 }
 
 func diffAndFilter(uni, uni2 uct.University) (filteredUniversity uct.University) {
+	if UniEqual(&uni, uni2) {
+		uni2.MainColor = ""
+		uni2.AccentColor = ""
+		uni2.HomePage = ""
+		uni2.RegistrationPage = ""
+		uni2.AvailableSemesters = []uct.Semester{}
+		uni2.Registrations = []uct.Registration{}
+		uni2.Metadata = []uct.Metadata{}
+	}
+
 	filteredUniversity = uni2
 	oldSubjects := uni.Subjects
 	newSubjects := uni2.Subjects
 	var filteredSubjects []uct.Subject
 	// For each newer subject
+
 	for s := range newSubjects {
+		Log(fmt.Sprintf("%s Subject index: %d \t %s | %s %s\n", oldSubjects[s].Season, s, oldSubjects[s].Name, newSubjects[s].Name, oldSubjects[s].Number == newSubjects[s].Number))
 		// If current index is out of range of the old subjects[] break and add every subject
 		if s >= len(oldSubjects) {
 			filteredSubjects = newSubjects
@@ -93,27 +108,33 @@ func diffAndFilter(uni, uni2 uct.University) (filteredUniversity uct.University)
 		}
 		// If newSubject != oldSubject, log why, then drill deeper to filter out ones tht haven't changed
 		if err := newSubjects[s].VerboseEqual(oldSubjects[s]); err != nil {
-			//uct.Log("Subject: ", err)
+			Log("Subject differs!")
 			oldCourses := oldSubjects[s].Courses
 			newCourses := newSubjects[s].Courses
 			var filteredCourses []uct.Course
 			for c := range newCourses {
+				Log(fmt.Sprintf("Course index: %d \t %s | %s %s\n", c, oldCourses[c].Name, newCourses[c].Name, oldCourses[c].Number == newCourses[c].Number))
+				if oldCourses[c].Number != newCourses[c].Number {
+					fmt.Printf("%s %s", oldCourses[c].Name, newCourses[c].Name)
+				}
+
 				if c >= len(oldCourses) {
 					filteredCourses = newCourses
 					break
 				}
 				if err := newCourses[c].VerboseEqual(oldCourses[c]); err != nil {
-					//uct.Log("Course: ",err)
+					Log("Course differs!")
 					oldSections := oldCourses[c].Sections
 					newSections := newCourses[c].Sections
 					var filteredSections []uct.Section
 					for e := range newSections {
-						if c >= len(oldCourses) {
+						Log(fmt.Sprintf("Section index: %d \t %s | %s %s\n", e, oldSections[e].Number, newSections[e].Number, oldSections[e].Number == newSections[e].Number))
+						if e >= len(oldSections) {
 							filteredSections = newSections
 							break
 						}
 						if err := newSections[e].VerboseEqual(oldSections[e]); err != nil {
-							uct.Log("Section: ", err)
+							Log("Section: ", err)
 							filteredSections = append(filteredSections, newSections[e])
 						}
 					}
@@ -127,4 +148,89 @@ func diffAndFilter(uni, uni2 uct.University) (filteredUniversity uct.University)
 	}
 	filteredUniversity.Subjects = filteredSubjects
 	return
+}
+
+func UniEqual(this *uct.University, that interface{}) bool {
+	if that == nil {
+		if this == nil {
+			return true
+		}
+		return false
+	}
+
+	that1, ok := that.(*uct.University)
+	if !ok {
+		that2, ok := that.(uct.University)
+		if ok {
+			that1 = &that2
+		} else {
+			return false
+		}
+	}
+	if that1 == nil {
+		if this == nil {
+			return true
+		}
+		return false
+	} else if this == nil {
+		return false
+	}
+	if this.Id != that1.Id {
+		return false
+	}
+	if this.Name != that1.Name {
+		return false
+	}
+	if this.Abbr != that1.Abbr {
+		return false
+	}
+	if this.HomePage != that1.HomePage {
+		return false
+	}
+	if this.RegistrationPage != that1.RegistrationPage {
+		return false
+	}
+	if this.MainColor != that1.MainColor {
+		return false
+	}
+	if this.AccentColor != that1.AccentColor {
+		return false
+	}
+	if this.TopicName != that1.TopicName {
+		return false
+	}
+	if len(this.AvailableSemesters) != len(that1.AvailableSemesters) {
+		return false
+	}
+	for i := range this.AvailableSemesters {
+		if !this.AvailableSemesters[i].Equal(&that1.AvailableSemesters[i]) {
+			return false
+		}
+	}
+	if len(this.Registrations) != len(that1.Registrations) {
+		return false
+	}
+	for i := range this.Registrations {
+		if !this.Registrations[i].Equal(&that1.Registrations[i]) {
+			return false
+		}
+	}
+	if len(this.Metadata) != len(that1.Metadata) {
+		return false
+	}
+	for i := range this.Metadata {
+		if !this.Metadata[i].Equal(&that1.Metadata[i]) {
+			return false
+		}
+	}
+	if !bytes.Equal(this.XXX_unrecognized, that1.XXX_unrecognized) {
+		return false
+	}
+	return true
+}
+
+func Log(v ...interface{}) {
+	if *verbose {
+		uct.Log(v)
+	}
 }
