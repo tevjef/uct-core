7082c6f7dd6bed0d848eae566a7bcb084f059b1c
Tevin Jeffrey <tev.jeffrey@gmail.com>
2016-10-29 03:13:14 -0400

    Semi stable njit scraper

--- scrapers/njit/main.go
+++ scrapers/njit/main.go
@@ -1,31 +1,54 @@
 package main
 
-import ()
 import (
+	"crypto/tls"
+	"encoding/json"
 	"fmt"
+	log "github.com/Sirupsen/logrus"
+	"github.com/satori/go.uuid"
+	"io/ioutil"
+	"net/http"
+	"net/http/cookiejar"
+	"net/url"
 	"strconv"
+	"strings"
 	"sync"
 	"time"
 	"uct/common/model"
-	"net/http"
-	log "github.com/Sirupsen/logrus"
-	"encoding/json"
+	"io"
+	"os"
 )
-
 
 func main() {
 
 	var university model.University
 
+	log.SetFormatter(&log.TextFormatter{})
 	university = njit
 
 	university.ResolvedSemesters = model.ResolveSemesters(time.Now(), university.Registrations)
 
-	_ = []*model.Semester{
+	semesters := []*model.Semester{
 		university.ResolvedSemesters.Last,
 		university.ResolvedSemesters.Current,
-		university.ResolvedSemesters.Next}
+		university.ResolvedSemesters.Next}[:1]
 
+	for _, semester := range semesters {
+		jar, _ = cookiejar.New(nil)
+
+		subjectRequest := subjectRequest{semester: *semester, paginated: paginated{offset: 1, max: 20}}
+		subjects := subjectRequest.requestSubjects()
+
+		for i := range subjects {
+			courseRequest := courseRequest{semester: *semester, subject: subjects[i].Code, paginated: paginated{offset: 0, max: 20}}
+			subjects[i].courses = courseRequest.requestSearch()
+		}
+
+		university.Subjects = append(university.Subjects, buildSubjects(subjects)...)
+	}
+
+	reader := model.MarshalMessage(model.JSON, university)
+	io.Copy(os.Stdout, reader)
 }
 
 const baseUrl = "https://myhub.njit.edu/StudentRegistrationSsb/ssb/"
@@ -33,7 +56,6 @@ const baseUrl = "https://myhub.njit.edu/StudentRegistrationSsb/ssb/"
 type paginated struct {
 	offset int
 	max    int
-	limit int
 	sync.Mutex
 }
 
@@ -42,52 +64,163 @@ type termRequest struct {
 }
 
 type subjectRequest struct {
-	term string
+	semester model.Semester
 	paginated
 }
 
-func (sr subjectRequest) buildUrl() string {
+func (sr *subjectRequest) buildUrl() string {
 	sr.Lock()
 	defer sr.Unlock()
-	return fmt.Sprintf("%sclassSearch/get_subject?term=%s&offset=%d&max=%d",
-		baseUrl, sr.term, sr.offset, sr.max)
+	return fmt.Sprintf("%sclassSearch/get_subject?term=%s&offset=%d&max=%d&searchTerm=&_=%d&startDatepicker=&endDatepicker=&sortColumn=subjectDescription",
+		baseUrl, parseSemester(sr.semester), sr.offset, sr.max, time.Now().Unix())
+}
+
+func (sr *subjectRequest) paginate() {
+	sr.Lock()
+	defer sr.Unlock()
+	sr.offset++
+}
+
+func (sr *subjectRequest) requestSubjects() (subjects []*NSubject) {
+	var url = sr.buildUrl()
+
+	page := []*NSubject{}
+
+	for firstPage := true; len(page) == sr.max || firstPage; {
+		page = []*NSubject{}
+		if err := getData(url, parseSemester(sr.semester), &page); err == nil {
+			for i := range subjects {
+				subject := subjects[i]
+				subject.season = sr.semester.Season
+				subject.year = int(sr.semester.Year)
+			}
+
+			subjects = append(subjects, page...)
+		}
+
+		firstPage = false
+		sr.paginate()
+		url = sr.buildUrl()
+	}
+
+	return
 }
 
 type courseRequest struct {
-	term    string
-	subject string
+	subject  string
+	semester model.Semester
 	paginated
 }
 
-func (cr courseRequest) buildUrl() string {
+func (cr *courseRequest) buildUrl() string {
 	cr.Lock()
 	defer cr.Unlock()
-	return fmt.Sprintf("%ssearchResults/searchResults?txt_subject=%s&txt_term=%spageOffset=%d&pageMaxSize=%d&sortDirection=asc",
-		baseUrl, cr.subject, cr.term, cr.offset, cr.max)
+	return fmt.Sprintf("%ssearchResults/searchResults?txt_subject=%s&txt_term=%s&pageOffset=%d&pageMax=%d&sortDirection=asc",
+		baseUrl, cr.subject, parseSemester(cr.semester), cr.offset, cr.max)
 }
+
+func (cr *courseRequest) paginate() {
+	cr.Lock()
+	defer cr.Unlock()
+	cr.offset+=cr.max
+	httpClient.PostForm("https://myhub.njit.edu/StudentRegistrationSsb/ssb/classSearch/resetDataForm", url.Values{})
+}
+
+func (cr *courseRequest) requestSearch() (courses []*NCourse) {
+	var url = cr.buildUrl()
+	page := NSearch{}
+	for firstPage := true; len(page.Data) >= cr.max || firstPage; {
+
+		page = NSearch{}
+		if err := getData(url, parseSemester(cr.semester), &page); err == nil {
+			courses = append(courses, page.Data...)
+		}
+
+		firstPage = false
+		cr.paginate()
+		url = cr.buildUrl()
+
+		log.Debugf("%s len in loop=%d offser=%d max=%d\n", cr.subject, len(page.Data), cr.offset, cr.max )
+	}
+
+	courses = cleanCourseList(courses)
+
+	return
+}
+
+var proxyUrl, _ = url.Parse("http://localhost:8888")
 
 var httpClient = &http.Client{
-	Timeout: 15 * time.Second,
+	Timeout:   15 * time.Second,
+	Jar:       jar,
+	Transport: &http.Transport{Proxy: http.ProxyURL(proxyUrl), TLSClientConfig: &tls.Config{InsecureSkipVerify: true}},
 }
 
-func getData(url string, model interface{}) error {
+var jar, _ = cookiejar.New(nil)
+
+var token = uuid.NewV4().String()
+
+func doRequest(rawUrl string) (*http.Response, error) {
+	req, _ := http.NewRequest(http.MethodGet, rawUrl, nil)
+
+	req.Header.Set("Accept", "application/json, text/javascript, */*; 0.01")
+	req.Header.Set("X-Requested-With", "XMLHttpRequest")
+	req.Header.Set("X-Synchronizer-Token", token)
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36")
+	req.Header.Set("Origin", "https://myhub.njit.edu")
+	if strings.Contains(rawUrl, "searchResults") || strings.Contains(rawUrl, "classSearch") {
+		req.Header.Set("Referer", "https://myhub.njit.edu/StudentRegistrationSsb/ssb/classSearch/classSearch")
+	}
+
+	return httpClient.Do(req)
+}
+
+var cookieMap = make(map[string]*http.Cookie)
+
+func getCookie(term string) *http.Cookie {
+	if cookieMap[term] == nil {
+		cookie := &http.Cookie{
+			Name:   "JSESSIONID",
+			Path:   "/StudentRegistrationSsb/",
+			Domain: "myhub.njit.edu",
+		}
+
+		resp, _ := httpClient.PostForm("https://myhub.njit.edu/StudentRegistrationSsb/ssb/term/search?mode=search", url.Values{"term": []string{term}})
+		if len(resp.Cookies()) > 0 && cookie.Value == "" {
+			cookie.Value = resp.Cookies()[0].Value
+			resp, _ = doRequest("https://myhub.njit.edu/StudentRegistrationSsb/ssb/classSearch/classSearch")
+		}
+
+		cookieMap[term] = cookie
+
+	}
+
+	return cookieMap[term]
+}
+
+func getData(rawUrl string, term string, model interface{}) error {
 	var success bool
 	modeType := fmt.Sprintf("%T", model)
-	req, _ := http.NewRequest(http.MethodGet, url, nil)
-	req.Header.Add("User-Agent", "Go/rutgers-scraper")
+
+	u, _ := url.Parse(rawUrl)
+	jar.SetCookies(u, []*http.Cookie{getCookie(term)})
+
 	for i := 0; i < 3; i++ {
 		startTime := time.Now()
-		log.WithFields(log.Fields{"retry": i, "url": url}).Debug(modeType + " request")
+		log.WithFields(log.Fields{"retry": i, "url": rawUrl}).Debug(modeType + " request")
 		time.Sleep(time.Duration(i*2) * time.Second)
-		resp, err := httpClient.Do(req)
+		resp, err := doRequest(rawUrl)
 		if err != nil {
 			log.Errorf("Retrying %d after error: %s\n", i, err)
 			continue
-		} else if err := json.NewDecoder(resp.Body).Decode(model); err != nil {
+		} else if data, err := ioutil.ReadAll(resp.Body); err != nil {
+			log.Errorf("Retrying %d after error: %s\n", i, err)
+			continue
+		} else if err := json.Unmarshal(data, model); err != nil {
 			log.Errorf("Retrying %d after error: %s\n", i, err)
 			continue
 		} else {
-			log.WithFields(log.Fields{"content-length": resp.ContentLength, "response_status": resp.StatusCode, "url": url, "response_time": time.Since(startTime).Seconds()}).Debug(modeType + " response")
+			log.WithFields(log.Fields{"content-length": len(data), "response_status": resp.StatusCode, "url": rawUrl, "response_time": time.Since(startTime).Seconds()}).Debug(modeType + " response")
 			success = true
 			break
 		}
@@ -95,8 +228,9 @@ func getData(url string, model interface{}) error {
 	}
 
 	if !success {
-		return fmt.Errorf("Unable to retrieve resource at %s", url)
+		return fmt.Errorf("Unable to retrieve resource at %s", rawUrl)
 	}
+
 	return nil
 }
 
@@ -104,10 +238,10 @@ func buildSubjects(njitSubjects []*NSubject) (s []*model.Subject) {
 
 	for _, subject := range njitSubjects {
 		newSubject := &model.Subject{
-			Name:   subject.Description,
-			Number: subject.Code,
-			Season: subject.season,
-			Year:   strconv.Itoa(subject.year),
+			Name:    subject.Description,
+			Number:  subject.Code,
+			Season:  subject.season,
+			Year:    strconv.Itoa(subject.year),
 			Courses: buildCourses(subject.courses),
 		}
 		s = append(s, newSubject)
@@ -115,11 +249,11 @@ func buildSubjects(njitSubjects []*NSubject) (s []*model.Subject) {
 	return
 }
 
-func buildCourses(njitCourses []NCourse) (c []*model.Course) {
+func buildCourses(njitCourses []*NCourse) (c []*model.Course) {
 	for _, course := range collapseCourses(njitCourses) {
 		newCourse := &model.Course{}
 		courseSection := course[0]
-		newCourse.Name = courseSection.CourseNumber
+		newCourse.Name = courseSection.CourseTitle
 		newCourse.Number = courseSection.CourseNumber
 		newCourse.Sections = buildSections(course)
 		c = append(c, newCourse)
@@ -128,7 +262,7 @@ func buildCourses(njitCourses []NCourse) (c []*model.Course) {
 	return
 }
 
-func buildSections(njitCourses []NCourse) (s []*model.Section) {
+func buildSections(njitCourses []*NCourse) (s []*model.Section) {
 	for _, course := range njitCourses {
 		newSection := &model.Section{}
 		newSection.Number = course.SequenceNumber
@@ -148,14 +282,14 @@ func buildSections(njitCourses []NCourse) (s []*model.Section) {
 	return
 }
 
-func buildMeetings(njitCourse NCourse) (meetings []*model.Meeting) {
+func buildMeetings(njitCourse *NCourse) (meetings []*model.Meeting) {
 	for _, val := range njitCourse.meetingTimes {
 		newMeeting := &model.Meeting{
 			StartTime: &val.BeginTime,
-			EndTime: &val.EndTime,
-			Room: &val.Room,
+			EndTime:   &val.EndTime,
+			Room:      &val.Room,
 			ClassType: &val.MeetingScheduleType,
-			Day: &val.day,
+			Day:       &val.day,
 		}
 
 		meetings = append(meetings, newMeeting)
@@ -164,7 +298,7 @@ func buildMeetings(njitCourse NCourse) (meetings []*model.Meeting) {
 	return
 }
 
-func buildInstructors(njitCourse NCourse) (instructors []*model.Instructor) {
+func buildInstructors(njitCourse *NCourse) (instructors []*model.Instructor) {
 	for _, val := range njitCourse.Faculty {
 		newInstructor := &model.Instructor{
 			Name: val.DisplayName,
--- scrapers/njit/njit.go
+++ scrapers/njit/njit.go
@@ -4,6 +4,8 @@ import (
 	"time"
 	"strconv"
 	"uct/common/model"
+	"github.com/Sirupsen/logrus"
+	"sort"
 )
 
 type NSemesters struct {
@@ -16,13 +18,13 @@ type NSubject struct {
 	Description string `json:"description"`
 	year        int
 	season      string
-	courses     []NCourse
+	courses     []*NCourse
 }
 
 type NSearch struct {
 	Success              bool      `json:"success"`
 	TotalCount           int       `json:"totalCount"`
-	Data                 []NCourse `json:"data"`
+	Data                 []*NCourse `json:"data"`
 	PageOffset           int       `json:"pageOffset"`
 	PageMaxSize          int       `json:"pageMaxSize"`
 	SectionsFetchedCount int       `json:"sectionsFetchedCount"`
@@ -55,7 +57,7 @@ type NCourse struct {
 	Faculty                 []Faculty         `json:"faculty"`
 	MeetingsFaculty         []MeetingsFaculty `json:"meetingsFaculty"`
 	status                  string
-	meetingTimes []MeetingTime
+	meetingTimes []*MeetingTime
 }
 
 type MeetingsFaculty struct {
@@ -107,14 +109,49 @@ type Faculty struct {
 
 func (c *NCourse) clean() {
 	c.status = parseStatus(*c)
-	c.meetingTimes = parseMeetingFaculty(c.MeetingsFaculty)
+	c.meetingTimes = c.parseMeetingFaculty(c.MeetingsFaculty)
+	for i := range  c.meetingTimes {
+		c.meetingTimes[i].clean()
+	}
+
+	sort.Sort(meetingByClass{c.meetingTimes})
+	sort.Sort(instructorSorter{c.Faculty})
 }
 
 func (m *MeetingTime) clean() {
 	m.day = parseDay(*m)
+	m.MeetingScheduleType = parseClassType(*m)
 	m.Room = parseRoom(*m)
-	m.StartDate = formatMeetingHour(m.StartDate)
+	m.BeginTime = formatMeetingHour(m.BeginTime)
 	m.EndTime = formatMeetingHour(m.EndTime)
+}
+
+func parseClassType(meetingTime MeetingTime) string {
+	switch meetingTime.MeetingScheduleType {
+	case "LEC":
+		return "Lecture"
+	case "STU":
+		return "Studio"
+	case "LAB":
+		return "Lab"
+	case "ADV":
+		return "Advising"
+	default:
+		return meetingTime.MeetingScheduleType
+	}
+}
+
+func (meetingTime MeetingTime) classRank() int {
+	if meetingTime.MeetingScheduleType == "Lecture" {
+		return 1
+	} else if meetingTime.MeetingScheduleType == "Studio" {
+		return 2
+	} else if meetingTime.MeetingScheduleType == "Lab" {
+		return 3
+	} else if meetingTime.MeetingScheduleType == "Advising" {
+		return 4
+	}
+	return 99
 }
 
 func formatMeetingHour(timeStr string) string {
@@ -180,63 +217,68 @@ func parseDay(m MeetingTime) string {
 	return ""
 }
 
-func parseMeetingFaculty(meetingFaculty []MeetingsFaculty) (meetingTimes []MeetingTime) {
-	for _, val := range meetingFaculty {
-		meetingTimes = append(meetingTimes, extractMeetings(val.MeetingTime)...)
+func (c *NCourse) parseMeetingFaculty(meetingFaculty []MeetingsFaculty) (meetingTimes []*MeetingTime) {
+	for i := range meetingFaculty {
+		meetingTimes = append(meetingTimes, c.extractMeetings(&meetingFaculty[i].MeetingTime)...)
 	}
 	return
 }
 
-func extractMeetings(njitMeeting MeetingTime) (meetings []MeetingTime) {
-	meeting := njitMeeting
+func (c *NCourse) extractMeetings(njitMeeting *MeetingTime) (meetings []*MeetingTime) {
+	meeting := *njitMeeting
 	meeting.Sunday, meeting.Monday, meeting.Tuesday, meeting.Wednesday, meeting.Thursday, meeting.Friday, meeting.Saturday = false, false, false, false, false, false, false
-	if meeting.Sunday {
+	if njitMeeting.Sunday {
 		n := meeting
 		n.Sunday = true
-		meetings = append(meetings, n)
+		meetings = append(meetings, &n)
 	}
 
-	if meeting.Monday {
+	if njitMeeting.Monday {
 		n := meeting
 		n.Monday = true
-		meetings = append(meetings, n)
+		meetings = append(meetings, &n)
 	}
 
-	if meeting.Tuesday {
+	if njitMeeting.Tuesday {
 		n := meeting
 		n.Tuesday = true
-		meetings = append(meetings, n)
+		meetings = append(meetings, &n)
 	}
 
-	if meeting.Wednesday {
+	if njitMeeting.Wednesday {
 		n := meeting
 		n.Wednesday = true
-		meetings = append(meetings, n)
+		meetings = append(meetings, &n)
 	}
 
-	if meeting.Thursday {
+	if njitMeeting.Thursday {
 		n := meeting
 		n.Thursday = true
-		meetings = append(meetings, n)
+		meetings = append(meetings, &n)
 	}
 
-	if meeting.Friday {
+	if njitMeeting.Friday {
 		n := meeting
 		n.Friday = true
-		meetings = append(meetings, n)
+		meetings = append(meetings, &n)
 	}
 
-	if meeting.Saturday {
+	if njitMeeting.Saturday {
 		n := meeting
 		n.Saturday = true
-		meetings = append(meetings, n)
+		meetings = append(meetings, &n)
+	}
+
+	if !njitMeeting.Sunday && !njitMeeting.Monday && !njitMeeting.Tuesday && !njitMeeting.Wednesday && !njitMeeting.Thursday && !njitMeeting.Friday && !njitMeeting.Saturday {
+		n := meeting
+		meetings = append(meetings, &n)
 	}
 
 	return
 }
 
-func collapseCourses(njitCourses []NCourse) (courses [][]NCourse) {
-	a := map[string][]NCourse{}
+func collapseCourses(njitCourses []*NCourse) (courses [][]*NCourse) {
+	a := map[string][]*NCourse{}
 
 	for i := range njitCourses {
 		key := njitCourses[i].CourseTitle
@@ -244,8 +286,192 @@ func collapseCourses(njitCourses []NCourse) (courses [][]NCourse) {
 	}
 
 	for _, val := range a {
+		sort.Sort(CourseSorter{val})
 		courses = append(courses, val)
 	}
 
+	sort.Sort(multiCourse{courses})
 	return
+}
+
+func cleanCourseList(njitCourses []*NCourse) (courses []*NCourse) {
+	hash := func(c NCourse) string {
+		return c.CourseTitle + c.CampusDescription + c.SubjectCourse + c.SubjectDescription + c.CourseNumber + c.SequenceNumber + c.CourseReferenceNumber + c.Term
+	}
+
+	var courseMap = make(map[string]*NCourse)
+
+	for i := range njitCourses {
+		c := njitCourses[i]
+		h := hash(*c)
+		if courseMap[h] == nil {
+			courseMap[h] = c
+		} else {
+			logrus.Debugln("Found duplicate", h)
+		}
+	}
+
+	for _, v := range courseMap {
+		v.clean()
+		courses = append(courses, v)
+	}
+
+	sort.Sort(CourseSorter{courses})
+
+	return
+}
+
+type CourseSorter struct {
+	Courses []*NCourse
+}
+
+func (a CourseSorter) Len() int {
+	return len(a.Courses)
+}
+
+func (a CourseSorter) Swap(i, j int) {
+	a.Courses[i], a.Courses[j] = a.Courses[j], a.Courses[i]
+}
+
+func (a CourseSorter) Less(i, j int) bool {
+	c1 := a.Courses[i]
+	c2 := a.Courses[j]
+
+	hash := func(c *NCourse) string {
+		return c.CourseNumber + c.CourseTitle + c.SequenceNumber + c.CourseReferenceNumber
+	}
+
+	return hash(c1) < hash(c2)
+}
+
+type multiCourse struct {
+	Courses [][]*NCourse
+}
+
+func (a multiCourse) Len() int {
+	return len(a.Courses)
+}
+
+func (a multiCourse) Swap(i, j int) {
+	a.Courses[i], a.Courses[j] = a.Courses[j], a.Courses[i]
+}
+
+func (a multiCourse) Less(i, j int) bool {
+	c1 := a.Courses[i][0]
+	c2 := a.Courses[j][0]
+
+	hash := func(c *NCourse) string {
+		return c.CourseNumber + c.CourseTitle + c.SequenceNumber + c.CourseReferenceNumber
+	}
+
+	return hash(c1) < hash(c2)
+}
+
+
+type meetingByClass struct {
+	Meeting []*MeetingTime
+}
+
+func (meeting meetingByClass) Len() int {
+	return len(meeting.Meeting)
+}
+
+func (meeting meetingByClass) Swap(i, j int) {
+	meeting.Meeting[i], meeting.Meeting[j] = meeting.Meeting[j], meeting.Meeting[i]
+}
+
+func (meeting meetingByClass) Less(i, j int) bool {
+	left, right := meeting.Meeting[i],  meeting.Meeting[j]
+
+	// Both have a day
+	if left.day != "" && right.day != "" {
+		if left.dayRank() < right.dayRank() {
+			return true
+		} else if left.dayRank() == right.dayRank() && left.BeginTime != "" && right.EndTime != "" {
+			return isAfter(left.BeginTime, right.EndTime)
+		}
+	}
+
+	// Neither have a day
+	if left.day == "" && right.day == "" {
+		return left.classRank() < right.classRank()
+	}
+
+	// One is missing their day
+	if left.dayRank() < right.dayRank() {
+		return true
+	} else if left.dayRank() == right.dayRank() {
+		return left.classRank() < right.classRank()
+	}
+
+	return false
+}
+
+type instructorSorter struct {
+	instructors []Faculty
+}
+
+func (a instructorSorter) Len() int {
+	return len(a.instructors)
+}
+
+func (a instructorSorter) Swap(i, j int) {
+	a.instructors[i], a.instructors[j] = a.instructors[j], a.instructors[i]
+}
+
+func (a instructorSorter) Less(i, j int) bool {
+	return a.instructors[i].DisplayName < a.instructors[j].DisplayName
+}
+
+func (meeting MeetingTime) dayRank() int {
+	switch meeting.day {
+	case "Monday":
+		return 1
+	case "Tuesday":
+		return 2
+	case "Wednesday":
+		return 3
+	case "Thurdsday":
+		return 4
+	case "Friday":
+		return 5
+	case "Saturday":
+		return 6
+	case "Sunday":
+		return 7
+	}
+	return 8
+}
+
+func isAfter(t1, t2 string) bool {
+	if length := len(t1); length == 7 {
+		t1 = "0" + t1
+	} else if length == 0 {
+		return false
+	}
+	if length := len(t2); length == 7 {
+		t2 = "0" + t2
+	} else if length == 0 {
+		return false
+	}
+
+	if t1[:2] == "12" {
+		t1 = t1[2:]
+		t1 = "00" + t1
+	}
+	if t2[:2] == "12" {
+		t2 = t2[2:]
+		t2 = "00" + t2
+	}
+
+	if t1[6:] == "AM" && t2[6:] == "PM" {
+		return true
+	} else if t1[6:] == "PM" && t2[6:] == "AM" {
+		return false
+	}
+
+	if t1[:2] == t2[:2] {
+		return t1[3:5] < t2[3:5]
+	}
+	return t1[:2] < t2[:2]
 }
