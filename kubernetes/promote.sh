1d46a5c402b5fbb2822a9bcc4c05191c92b9be22
Tevin Jeffrey <tev.jeffrey@gmail.com>
2017-09-14 22:51:23 -0400

    Make metric monitors target the correct namespace in production

--- kubernetes/promote.sh
+++ kubernetes/promote.sh
@@ -5,6 +5,8 @@ cp -R staging production
 
 find -E "production" -regex ".*\.(yml|yaml)" -print0 | xargs -0 sed -i '' 's/namespace: staging/namespace: production/g'
 find -E "production" -regex ".*\.(yml|yaml)" -print0 | xargs -0 sed -i '' 's/:staging/:production/g'
+find -E "production" -regex ".*\.(yml|yaml)" -print0 | xargs -0 sed -i '' 's/- staging/- production/g'
+find -E "production" -regex ".*\.(yml|yaml)" -print0 | xargs -0 sed -i '' 's/.staging/.production/g'
 
 # Clean up if there has been no errors
 rm -r production-bak
