b70e2ef6fba85720a50819d922d77a2b15d6e3ea
Tevin Jeffrey <tev.jeffrey@gmail.com>
2017-11-26 23:22:10 -0500

    Update notes on backing up the database

--- kubernetes/staging/postgres/backup-restore-notes.sh
+++
@@ -1,3 +0,0 @@
-PGPASSWORD=passs pg_dump --host=foreman.tevindev.me -U universityct -v -w -c -Z 9 > universityct.sql.gz
-
-cat universityct.sql.gz | gunzip |  psql --host=localhost -U universityct -d universityct
--- kubernetes/staging/postgres/drop_db.sql
+++
@@ -1,9 +0,0 @@
--- Disallow new connections
-UPDATE pg_database SET datallowconn = 'false' WHERE datname = 'universityct';
-ALTER DATABASE universityct CONNECTION LIMIT 1;
-
--- Terminate existing connections
-SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'universityct';
-
--- Drop database
-DROP DATABASE universityct;
