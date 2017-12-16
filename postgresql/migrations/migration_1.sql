9f3b02062c7369fb749efd9f9f7b00099105bd55
Tevin Jeffrey <tev.jeffrey@gmail.com>
2017-12-16 18:46:44 -0500

    Move postgresql build location

---
+++ postgresql/migrations/migration_1.sql
@@ -0,0 +1,56 @@
+CREATE TYPE os AS ENUM (
+  'android',
+  'ios'
+);
+
+CREATE TABLE public.subscription
+(
+  id SERIAL,
+  os os,
+  os_version TEXT,
+  app_version TEXT,
+  is_subscribed BOOLEAN NOT NULL,
+  topic_name TEXT NOT NULL,
+  fcm_token TEXT NOT NULL,
+  created_at TIMESTAMP,
+  updated_at TIMESTAMP,
+  CONSTRAINT subscription__pk PRIMARY KEY (id)
+);
+
+CREATE TRIGGER insert_subscription_time_stamps
+BEFORE INSERT ON public.subscription
+FOR EACH ROW
+EXECUTE PROCEDURE update_row_time_stamp();
+
+CREATE TRIGGER update_subscription_time_stamps
+BEFORE UPDATE ON public.subscription
+FOR EACH ROW
+WHEN (OLD.* IS DISTINCT FROM NEW.*)
+EXECUTE PROCEDURE update_row_time_stamp();
+
+CREATE TABLE public.acknowledge
+(
+  id SERIAL,
+  notification_id BIGINT NOT NULL,
+  os os,
+  os_version TEXT,
+  app_version TEXT,
+  topic_name TEXT NOT NULL,
+  fcm_token TEXT NOT NULL,
+  receive_at TIMESTAMP NOT NULL,
+  created_at TIMESTAMP,
+  updated_at TIMESTAMP,
+  CONSTRAINT acknowledge__pk PRIMARY KEY (id),
+  CONSTRAINT acknowledge_university__fk FOREIGN KEY (notification_id) REFERENCES public.notification (id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TRIGGER insert_message_time_stamps
+BEFORE INSERT ON public.acknowledge
+FOR EACH ROW
+EXECUTE PROCEDURE update_row_time_stamp();
+
+CREATE TRIGGER update_message_time_stamps
+BEFORE UPDATE ON public.acknowledge
+FOR EACH ROW
+WHEN (OLD.* IS DISTINCT FROM NEW.*)
+EXECUTE PROCEDURE update_row_time_stamp();
